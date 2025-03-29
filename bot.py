7837843489078aeeff1c20cbc496550fab7911d0
list242 <matvej.v@list.ru>
2025-03-29 17:19:00 +0300

    убираю запуск через сеть

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from handlers.boat_handler import boat_handler
 async def main():
     try:
         # Вставьте сюда токен вашего бота
-        token = "7933616069:AAE1rIpYDIehi3h5gYFU7UQizeYhCifbFRk"
+        token = "YOUR_BOT_TOKEN"
         if not token or token == "YOUR_BOT_TOKEN":
             raise ValueError("Токен бота не установлен. Пожалуйста, укажите действительный токен.")
 
@@ -22,20 +22,11 @@ async def main():
         application.add_handler(help_handler)
         application.add_handler(back_handler)
 
-        # Настройка вебхуков
-        WEBHOOK_URL = "https://your-railway-domain.up.railway.app/webhook"  # Замените на ваш домен Railway
-        await application.bot.set_webhook(url=WEBHOOK_URL)
-
         # Запускаем бота
         print("Бот запущен...")
         await application.initialize()
         await application.start()
-        
-        await application.updater.start_webhook(
-            listen="0.0.0.0",  # Слушаем все входящие подключения
-            port=8080,         # Порт, указанный в railway.yml
-            url_path="/webhook"  # Путь для вебхука
-        )
+        await application.updater.start_polling()
         print("Бот работает. Нажмите Ctrl+C для завершения.")
         while True:
             await asyncio.sleep(1)
