013a93a6dbc81840c9845b93f095295855b8ce8d
list242 <matvej.v@list.ru>
2025-04-05 21:39:46 +0300

    убран

--- bot.py
+++ bot.py
@@ -28,5 +28,6 @@ if __name__ == '__main__':
     application.run_webhook(
         listen="0.0.0.0",
         port=int(os.getenv("PORT", 8000)),
-        webhook_url=f"{WEBHOOK_URL}/telegram"
+        webhook_url=f"{WEBHOOK_URL}/"
+
     )
